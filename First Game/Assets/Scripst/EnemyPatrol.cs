65e6263f7722efdf4f9a4c381a888e328544fd62
Cristian Monzon Guzman <Cristian-02-@live.com>
2020-10-27 11:24:42 -0500

    Creacion del mapa y mecanicas y animaciones del enemigo y jugador

---
+++ First Game/Assets/Scripst/EnemyPatrol.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+
+    public float speed = 1f;
+    public float minX;
+    public float maxX;
+    public float waitingTime = 2f;
+
+    private GameObject _target;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateTarget();
+        StartCoroutine("PatrolToTarget");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void UpdateTarget()
+    {
+        // Si en el primer tiempo creamos el target en la izquierda
+        if(_target == null)
+        {
+            _target = new GameObject("Target");
+            _target.transform.position = new Vector2(minX, transform.position.y);
+            transform.localScale = new Vector3(-1, 1, 1);
+            return;
+        }
+
+        // Si nosostros estamos en la izquierda, cambiamos el target a la derecha
+        if(_target.transform.position.x == minX)
+        {
+            _target.transform.position = new Vector2(maxX, transform.position.y);
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        // Si nosostros estamos en la derecha, cambiamos el target a la izquierda
+        else if (_target.transform.position.x == maxX)
+        {
+            _target.transform.position = new Vector2(minX, transform.position.y);
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+    // Courotine para mover al enemigo
+    private IEnumerator PatrolToTarget()
+    {
+        while(Vector2.Distance(transform.position, _target.transform.position) > 0.05f)
+        {
+            Vector2 direction = _target.transform.position - transform.position;
+            float xDirection = direction.x;
+
+            transform.Translate(direction.normalized * speed * Time.deltaTime);
+
+            yield return null; 
+        }
+
+        // En este punto he alcanzado al target, vamos a setear nuestra posicion 
+        //Debug.Log("Target alcanzado");
+        transform.position = new Vector2(_target.transform.position.x, transform.position.y);
+
+        // Y vamos a esperar un momento 
+        //Debug.Log("Esperamos por " + waitingTime + "Segundos");
+        yield return new WaitForSeconds(waitingTime);
+
+        // Una vez esperando camos a restaurar el patrol
+        //Debug.Log("Espera terminada, vamos a movernos de nuevo");
+        UpdateTarget();
+        StartCoroutine("PatrolToTarget");
+    }
+}
---
+++ First Game/Assets/Scripst/PlayerController.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Definimos una variable de tipo float que determinara la velocidad
+    public float speed = 2.5f;
+    public float jumpForce = 2.5f;
+
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    public float groundCheckRadius;
+
+    // Creamos dos varibles uno de tipo Rigidbody2D y otra de tipo Animator 
+    private Rigidbody2D _rigidbody;
+    private Animator _animator;
+
+    // Creamos una variable de tipo vector que determinara el movimiento
+    private Vector2 _movement;
+    private bool _facingRight = true;
+    private bool _isGrounded;
+
+    // Atacar
+    private bool _isAttacking;
+
+    private void Awake()
+    {
+        // Obtenemos los componenetes Rigidbody2D y Animator
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Movimiento 
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        _movement = new Vector2(horizontalInput, 0f);
+
+        // Flip
+        if (horizontalInput < 0f && _facingRight == true)
+        {
+            Flip();
+        }
+        else if (horizontalInput > 0f && _facingRight == false)
+        {
+            Flip();
+        }
+
+        //Esta suelo??
+       _isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        //Debug.Log("Salto");
+        //Esta saltando??
+        if (Input.GetButtonDown("Jump") && _isGrounded == false)
+        {
+            //Debug.Log("Salto");
+            _rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+        // Quiero Atacar??
+        if(Input.GetButtonDown("Fire1") && _isGrounded == false && _isAttacking == false)
+        {
+            //Debug.Log("Ataco");
+            _movement = Vector2.zero;
+            _rigidbody.velocity = Vector2.zero;
+            _animator.SetTrigger("Attack");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontalVelocity = _movement.normalized.x * speed;
+        _rigidbody.velocity = new Vector2(horizontalVelocity, _rigidbody.velocity.y);
+
+    }
+
+    private void LateUpdate()
+    {
+        _animator.SetBool("Idle", _movement == Vector2.zero);
+        _animator.SetBool("IsGrounded", _isGrounded);
+        _animator.SetFloat("VerticalVelocity", _rigidbody.velocity.y);
+
+        // Animator 
+        if (_animator.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))
+        {
+            _isAttacking = true;
+
+        }
+        else
+        {
+            _isAttacking = false;
+        }
+    }
+
+    private void Flip()
+    {
+        _facingRight = !_facingRight;
+
+        float localScaleX = transform.localScale.x;
+
+        localScaleX = localScaleX * -1f;
+
+        transform.localScale = new Vector3(localScaleX, transform.localScale.y, transform.localScale.z);
+
+    }
+}
